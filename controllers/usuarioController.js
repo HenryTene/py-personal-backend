d29a4b65a01ecfaecffe330f96ed4fd93f2e04dc
Henry Tene <henry_tene@hotmail.com>
2022-05-19 12:35:57 -0500

    Se añade funcion para reestablecer password

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,7 +1,7 @@
 import Usuario from "../models/Usuario.js";
 import generarId from "../helpers/generarId.js";
 import generarJWT from "../helpers/generarJWT.js";
-import { emailRegistro } from "../helpers/email.js";
+import { emailRegistro, emailOlvidePassword } from "../helpers/email.js";
 
 const registrar = async (req, res) => {
   //Evitar registros duplicados
@@ -95,6 +95,13 @@ const olvidePassword = async (req, res) => {
   try {
     usuario.token = generarId();
     await usuario.save();
+    //Enviar el email de confirmacion
+    emailOlvidePassword({
+      email: usuario.email,
+      nombre: usuario.nombre,
+      token: usuario.token,
+    });
+
     res.json({ msg: "Se ha enviado un email con las instrucciones" });
   } catch (error) {
     console.log(error);
--- helpers/email.js
+++ helpers/email.js
@@ -2,6 +2,7 @@ import nodemailer from "nodemailer";
 
 export const emailRegistro = async (datos) => {
   const { nombre, email, token } = datos;
+  //TODO:mover hacia variables de entorno
   const transport = nodemailer.createTransport({
     host: "smtp.mailtrap.io",
     port: 2525,
@@ -18,5 +19,28 @@ export const emailRegistro = async (datos) => {
     text: "Confirma tu cuenta en simple Project",
     html: `<p>Hola ${nombre}, para confirmar tu cuenta en Simple Project,</p> 
     <p> haz click en el siguiente enlace:<a href="${process.env.FRONTEND_URL}/confirmar/${token}">Comprobar cuenta</a> </p><p>Si no creaste esta cuenta puedes ignorar este mensaje</p>`,
+  });
+};
+
+export const emailOlvidePassword = async (datos) => {
+  const { nombre, email, token } = datos;
+  //TODO:mover hacia variables de entorno
+  const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "53c43f81388eba",
+      pass: "47c28c43a11977",
+    },
+  });
+  //Informacion del email
+
+  const info = await transport.sendMail({
+    from: '"Simple Project-Adminstardor de Proyectos" <cuenta@simpleproject.com>',
+    to: email,
+    subject: "Simple Project - Reestablece tu Password",
+    text: "Reestablece tu Password",
+    html: `<p>Hola ${nombre}, solicitaste reestablecer tu password,</p> 
+    <p> haz click en el siguiente enlace para: <a href="${process.env.FRONTEND_URL}/olvide-password/${token}">reestablecer el password</a> </p><p>Si no solicitaste este cambio puedes ignorar este mensaje</p>`,
   });
 };
